166b5a8ac1591bb30b49fdee6cb0463766a41bac
cjshaw <cjshaw@mit.edu>
2016-10-28 14:57:42 -0400

    add header for CORS methods to enable put / post / delete

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "Options, PUT, POST, DELETE, GET");
   next();
 });
 
