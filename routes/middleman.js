70f533ae574f39dcbed270fbecc1a6ca1cad61f7
cjshaw <cjshaw@mit.edu>
2016-12-20 09:53:23 -0500

    use sharedBankAlias for phase i results

--- routes/middleman.js
+++ routes/middleman.js
@@ -602,7 +602,7 @@ function getMissionResults(req, res) {
   } else {
 
     options = {
-      path: `assessment/banks/${req.params.bankId}/assessmentsoffered/${req.params.offeredId}/results?raw&isolated`
+      path: `assessment/banks/${sharedBankAlias(req.params.bankId)}/assessmentsoffered/${req.params.offeredId}/results?raw&isolated`
     }
   }
   console.log('options', options)
