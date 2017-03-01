198cafecbb77039ff3ea11c2564ede1579dc8f21
cjshaw <cjshaw@mit.edu>
2017-03-01 14:26:32 -0500

    update credentials file

--- credentials/index.js
+++ credentials/index.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   d2l: {
-    appID: process.env.D2L_APP_ID || '',
-    appKey: process.env.D2L_APP_KEY || '',
-    host: process.env.D2L_HOST || 'example.com',
+    appID: process.env.PROD_D2L_APP_ID || process.env.DEV_D2L_APP_ID || '',
+    appKey: process.env.PROD_D2L_APP_KEY || process.env.DEV_D2L_APP_KEY || '',
+    host: process.env.PROD_D2L_HOST || process.env.DEV_D2L_HOST || 'example.com',
     port: 443
   },
   login: 'simple',  // simple, d2l
@@ -15,14 +15,14 @@ module.exports = {
     Host: process.env.PROD_MC3_HOST || process.env.DEV_MC3_HOST || 'example.com'
   },
   qbank: {
-    SecretKey: process.env.QBANK_SECRET || '',
-    AccessKeyId: process.env.QBANK_ACCESS_ID || '',
-    Host: process.env.QBANK_HOST || 'example.com',
+    SecretKey: process.env.PROD_QBANK_SECRET || process.env.DEV_QBANK_SECRET || '',
+    AccessKeyId: process.env.PROD_QBANK_ACCESS_ID || process.env.DEV_QBANK_ACCESS_ID || '',
+    Host: process.env.PROD_QBANK_HOST || process.env.DEV_QBANK_HOST || 'example.com',
     SchoolNodes: {
       'acc': 'assessment.Bank%3A57279fc2e7dde08807231e61%40bazzim.MIT.EDU',
       'qcc': 'assessment.Bank%3A57279fcee7dde08832f93420%40bazzim.MIT.EDU'
     },
-    Proxy: process.env.QBANK_PROXY || ''
+    Proxy: process.env.PROD_QBANK_PROXY || process.env.DEV_QBANK_PROXY || ''
   },
   KatexURL: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.js',
   KatexCSS: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.6.0/katex.min.css',
